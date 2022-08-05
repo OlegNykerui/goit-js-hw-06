49b2c62e99b42581da59cc2948c74e40e3860f0b
Oleg Nykerui <nykeruyoleg96@gmail.com>
2022-08-05 18:17:41 +0300

    add 2 and 3 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listItem = document.getElementById("ingredients");
+ingredients.forEach((element) => {
+  const createItem = document.createElement("li");
+  createItem.textContent += element;
+  createItem.classList.add("item");
+  listItem.append(createItem);
+});
+console.log(listItem);
